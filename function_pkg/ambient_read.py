1591d6039992aabaa33638ae7df4d0ac75e08ef7
iput2023-takeuchia <iput2023takeuchia@gmail.com>
2025-10-02 13:16:43 +0900

    [update] 非公開ボードの検証

--- function_pkg/ambient_read.py
+++ function_pkg/ambient_read.py
@@ -4,10 +4,13 @@ import threading
 import json
 import data_csv
 
-AMBIENT_CHANNEL_ID_PUBLIC ="91488"
-AMBIENT_READ_KEY_PUBLIC = "ec1c09455dc8fd8d"
+
 AMBIENT_CHANNEL_ID_TEST = "92194"             # 家庭土
 AMBIENT_READ_KEY_TEST = "be75dfc419277287"
+AMBIENT_CHANNEL_ID_PRIVATE = "91500"
+AMBIENT_READ_KEY_PRIVATE = "dd0f7379df494d15"
+AMBIENT_CHANNEL_ID_PUBLIC ="91488"
+AMBIENT_READ_KEY_PUBLIC = "ec1c09455dc8fd8d"
 
 
 # amb = ambient.Ambient(AMBIENT_CHANNEL_ID_TEST, '' , AMBIENT_READ_KEY_TEST)
@@ -18,16 +21,16 @@ AMBIENT_READ_KEY_TEST = "be75dfc419277287"
 
 def get_ambient_data():
     stop_event = threading.Event()
-    url = f"https://ambidata.io/api/v2/channels/{AMBIENT_CHANNEL_ID_TEST}/data"
-    params = {"readKey": AMBIENT_READ_KEY_TEST, "n": 10}  # 最新10件を取得
+    url = f"https://ambidata.io/api/v2/channels/{AMBIENT_CHANNEL_ID_PRIVATE}/data"
+    params = {"readKey": AMBIENT_READ_KEY_PRIVATE, "n": 10}  # 最新10件を取得
     r = requests.get(url, params=params)
     print(r.status_code)  # 200 が返ればOK
-    list = r.json()       # JSONデータ表示
-    data_csv.csv_write_iterator(list)
+    list = r.json()
+    print(list)       # JSONデータ表示
+    # data_csv.csv_write_iterator(list)   #CSVファイルへの書き込み
 
 
 
 if __name__ == '__main__':
     print("Start if __name__ == '__main__'")
     data_r_list = get_ambient_data()
-    print(data_r_list)
--- web_show.py
+++ web_show.py
@@ -1,23 +1,22 @@
 from flask import Flask, render_template
-import function_pkg.data_csv as data_csv
+import function_pkg.data_csv as csv
+import function_pkg.ambient_read as amb
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def display_csvdata():
     print("Hello! (to Terminal)")
-
-    data_csv_list = data_csv.csv_read_iterator()
+    amb.get_ambient_data()
+    data_csv_list = csv.csv_read_iterator()
     print("data_csv_list: ", data_csv_list)
     
     return render_template("data_show.html",  input_ambient_data=data_csv_list) 
 
-# @app.route("/update", methods=["POST"])
-# def display_csvdata():
-    
-
-#     data_csv_list = data_csv.csv_read_iterator()
-#     print("data_csv_list: ", data_csv_list)
+@app.route("/update", methods=["POST"])
+def reload_ambient_data():
+    data_csv_list = csv.csv_read_iterator()
+    print("data_csv_list: ", data_csv_list)
     
 if __name__ == '__main__':
     print("Start if __name__ == '__main__'")
